9ccd5f0796789082e28e93cfeb81c1fdd78a27ca
Sébastien Verbois <sebastien.verbois@imio.be>
2021-12-12 11:49:50 +0100

    Add row-edit-toolbar and row redirect view

---
+++ src/collective/contentsections/contents/row/views.py
@@ -0,0 +1,10 @@
+from Products.Five.browser import BrowserView
+
+
+class RowView(BrowserView):
+    """Row view"""
+
+    def __call__(self):
+        page = self.context.__parent__
+        url = f"{page.absolute_url()}#row-{self.context.id}"
+        self.request.response.redirect(url)
