04647bfdb69463f9f6e08535190ac9ccafdfb2a6
Sébastien Verbois <sebastien.verbois@imio.be>
2022-08-21 11:41:42 +0200

    Back to development: 1.0.0a14

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = "\n\n".join(
 )
 setup(
     name="collective.contentsections",
-    version="1.0.0a13",
+    version="1.0.0a14.dev0",
     description="A block approach for Plone 6 Classic",
     long_description=long_description,
     long_description_content_type="text/markdown",
